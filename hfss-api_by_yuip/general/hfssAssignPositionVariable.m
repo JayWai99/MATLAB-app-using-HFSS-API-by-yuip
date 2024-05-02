49979a7e6123040778f3ef61a8f4af46b9c9abe0
JayWai99 <jaydencwk@gmail.com>
2024-05-02 14:02:49 +1000

    More files from hfss

---
+++ hfss-api_by_yuip/general/hfssAssignPositionVariable.m
@@ -0,0 +1,70 @@
+% ----------------------------------------------------------------------------
+% function hfssAssignPositionVariable(fid, Name, type, Axis, variable)
+% 
+% Description :
+% -------------
+% Create the VB Script necessary to assign a local variable in HFSS.
+%
+% Parameters :
+% ------------
+% fid      - File identifier of the HFSS script file.
+% Name     - Name of the object to which the variable will be assigned.
+% type     - Type of object. Currently supported: 'Rectangle', 'Box' and
+%            'SweepAlongVector'.
+% Axis     - Axis where the variable is assigned.
+% variable - The name of the variable to be assigned.
+%
+% Example :
+% ---------
+% fid = fopen('myantenna.vbs', 'wt');
+% ... 
+% hfssAssignSizeVariable(fid, 'Patch', 'Rectangle', 'X', 'var1');
+%
+% ----------------------------------------------------------------------------
+
+% ----------------------------------------------------------------------------
+% CHANGELOG
+%
+% 02-Sep-2020: *Initial release (DRP).
+% 08-Sep-2020: *Added Box type (DRP).
+% 21-Apr-2021: *Added support for type SweepAlongVector (DRP).
+% ----------------------------------------------------------------------------
+
+% ----------------------------------------------------------------------------
+% Written by Daniel Rodriguez Prado
+% danysan@gmail.com
+% 02 September 2020
+% ----------------------------------------------------------------------------
+function hfssAssignPositionVariable(fid, Name, type, variables)
+	% Preamble.
+	fprintf(fid, '\n');
+
+	% Change the variable name
+	fprintf(fid, 'oEditor.ChangeProperty _\n');
+	fprintf(fid, 'Array("NAME:AllTabs", _\n');
+	fprintf(fid, 'Array("NAME:Geometry3DCmdTab", _\n');
+	fprintf(fid, 'Array("NAME:PropServers",  _\n');
+    
+    switch type
+        case 'Rectangle'
+            fprintf(fid, '"%s:CreateRectangle:1"), _\n', Name);
+        case 'Box'
+            fprintf(fid, '"%s:CreateBox:1"), _\n', Name);
+        case 'SweepAlongVector'
+            fprintf(fid, '"%s:SweepAlongVector:1"), _\n', Name);
+        otherwise
+            error('Type not supported!');
+    end
+    
+	fprintf(fid, 'Array("NAME:ChangedProps", _\n');
+    
+    switch type
+        case 'SweepAlongVector'
+            fprintf(fid, 'Array("NAME:Vector", _\n');
+        otherwise
+            fprintf(fid, 'Array("NAME:Position", _\n');
+    end
+    
+    fprintf(fid, '"X:=", "%s", _\n', variables{1});
+    fprintf(fid, '"Y:=", "%s", _\n', variables{2});
+    fprintf(fid, '"Z:=", "%s"))))\n', variables{3});
---
+++ hfss-api_by_yuip/general/hfssSetVariable.m
@@ -0,0 +1,56 @@
+% ----------------------------------------------------------------------------
+% function function hfssSetVariable(fid, variable, value, [units])
+% 
+% Description :
+% -------------
+% Create the VB Script necessary to create a new local variable in HFSS.
+%
+% Parameters :
+% ------------
+% fid      - file identifier of the HFSS script file.
+% variable - The name of the variable to be created.
+% value    - The value for the variable (DOUBLE or CHAR).
+% units    - (Optional) units of the variable (specify using either 'in',
+%            'mm', 'meter' or anything else defined in HFSS).
+%
+% Example :
+% ---------
+% fid = fopen('myantenna.vbs', 'wt');
+% ... 
+% hfssSetVariable(fid, 'radius', 5, 'cm');
+% hfssSetVariable(fid, 'diameter', '2*radius');
+%
+% ----------------------------------------------------------------------------
+
+% ----------------------------------------------------------------------------
+% CHANGELOG
+%
+% 28-Sep-2015: *Initial release (RS).
+% 02-Sep-2020: *Allows to assign a variable as a function of other
+%               variables (DRP).
+% ----------------------------------------------------------------------------
+
+% ----------------------------------------------------------------------------
+% Written by Rounak Singh
+% rounaksingh17@gmail.com
+% 28 September 2015
+% ----------------------------------------------------------------------------
+function hfssSetVariable(fid, variable, value, units)
+	% Preamble.
+	fprintf(fid, '\n');
+
+	% Change the variable name
+	fprintf(fid, 'oDesign.ChangeProperty _\n');
+	fprintf(fid, 'Array("NAME:AllTabs", _\n');
+	fprintf(fid, 'Array("NAME:LocalVariableTab", _\n');
+	fprintf(fid, 'Array("NAME:PropServers",  _\n');
+	fprintf(fid, '"LocalVariables"), _\n');
+	fprintf(fid, 'Array("NAME:NewProps", _\n');
+	fprintf(fid, 'Array("NAME:%s", _\n', variable);
+    fprintf(fid, '"PropType:=", "VariableProp", _\n');
+    fprintf(fid, '"UserDef:=", true, _\n');
+    if ischar(value)
+        fprintf(fid, '"Value:=", "%s"))))\n', value);
+    else
+        fprintf(fid, '"Value:=", "%f%s"))))\n', value, units);
+    end
